89c78ebc30905030a92138baedbfea7373ce330c
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-03-29 12:19:02 +0600

    react-router-dom setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Pages/Home.jsx";
+import ListedBooks from "./Pages/ListedBooks.jsx";
+import PagesToRead from "./Pages/PagesToRead.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+  {
+    path: "/listedbooks",
+    element: <ListedBooks></ListedBooks>
+  },
+  {
+    path:"/pagestoread",
+    element: <PagesToRead></PagesToRead>
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
