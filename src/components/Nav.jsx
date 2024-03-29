31efed547f79a8736280a72bd3895a0d2794fb11
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-03-29 12:45:30 +0600

    Navlink route added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,39 +1,64 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-white">
       <div className="navbar-start">
         <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+          <div tabIndex={0} role="button" className="btn  lg:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
             </svg>
           </div>
-          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-white rounded-box w-52">
             <li>
-              <a>Home</a>
+              <NavLink to="/" className={({ isActive }) => (isActive ? "text-green-400 border-green-400 border-2 rounded-md" : "font-semibold")}>
+                Home
+              </NavLink>
             </li>
             <li>
-              <a>Listed Books</a>
+              <NavLink
+                to="/listedbooks"
+                className={({ isActive }) => (isActive ? "text-green-400 border-green-400 border-2 rounded-md" : "font-semibold")}>
+                Listed Books
+              </NavLink>
             </li>
             <li>
-              <a>Pages to Read</a>
+              <NavLink
+                to="/pagestoread"
+                className={({ isActive }) => (isActive ? "text-green-400 border-green-400 border-2 rounded-md" : "font-semibold")}>
+                Pages To Read
+              </NavLink>
             </li>
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">Book Nest</a>
+        <Link to="/" className="btn btn-ghost text-xl">
+          Book Nest{" "}
+        </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
-          <li className="font-semibold text-green-400 border-green-400 border-2 rounded-md">
-            <a>Home</a>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? "text-green-400 border-green-400 border-2 rounded-md font-semibold" : "font-semibold")}>
+              Home
+            </NavLink>
           </li>
-          <li className="font-semibold">
-            <a>Listed Books</a>
+          <li>
+            <NavLink
+              to="/listedbooks"
+              className={({ isActive }) => (isActive ? "text-green-400 border-green-400 border-2 rounded-md font-semibold" : "font-semibold")}>
+              Listed Books
+            </NavLink>
           </li>
-          <li className="font-semibold">
-            <a>Pages to Read</a>
+          <li>
+            <NavLink
+              to="/pagestoread"
+              className={({ isActive }) => (isActive ? "text-green-400 border-green-400 border-2 rounded-md font-semibold" : "font-semibold")}>
+              Pages To Read
+            </NavLink>
           </li>
         </ul>
       </div>
