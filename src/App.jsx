576e6d19f9fbd20ab26382bdf01c8e0d1395bbbc
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-03-29 12:07:15 +0600

    ListedBooks and PagesToRead pages created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import './App.css'
-import Home from './Pages/Home'
+import "./App.css";
+import Home from "./Pages/Home";
+import ListedBooks from "./Pages/ListedBooks";
+import PagesToRead from "./Pages/PagesToRead";
 
 function App() {
   return (
     <>
-<Home></Home>
+      <Home></Home>
+      <ListedBooks></ListedBooks>
+      <PagesToRead></PagesToRead>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
